7aa71ddd4b795bc9deffc0d92cb95ffabc5f5279
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 20:44:40 +0530

    by default ts transpiles to es5

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function log(message) {
-    console.log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally: " + i);
 }
-var message = "Hello World";
-log(message);
+doSomething();
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-function log(message) {
-  console.log(message);
+function doSomething() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+
+  console.log("Finally: " + i);
 }
 
-var message = "Hello World";
-log(message);
+doSomething();
