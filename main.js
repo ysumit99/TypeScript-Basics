7b1d54a0985e577405b0a173b4bfdccf91fec873
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 21:25:48 +0530

    setters and getters

--- main.js
+++ main.js
@@ -6,7 +6,21 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log("X: " + this.x + ", Y: " + this.y);
     };
+    Object.defineProperty(Point.prototype, "X", {
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("value cannot be less than 0.");
+            this.x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point(1, 2);
+var x = point.X;
+point.X = 10;
 point.draw();
--- main.ts
+++ main.ts
@@ -4,7 +4,18 @@ class Point {
   draw() {
     console.log("X: " + this.x + ", Y: " + this.y);
   }
+
+  get X() {
+    return this.x;
+  }
+
+  set X(value) {
+    if (value < 0) throw new Error("value cannot be less than 0.");
+    this.x = value;
+  }
 }
 
 let point = new Point(1, 2);
+let x = point.X;
+point.X = 10;
 point.draw();
