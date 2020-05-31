ee8fb524375c7ad071d293fb4fa860747405f9ca
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 21:15:06 +0530

    constructor and optional parameter

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 class Point {
   x: number;
   y: number;
+
+  constructor(x?: number, y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+
   draw() {
     console.log("X: " + this.x + ", Y: " + this.y);
   }
@@ -11,6 +17,4 @@ class Point {
 }
 
 let point = new Point();
-point.x = 1;
-point.y = 3;
 point.draw();
