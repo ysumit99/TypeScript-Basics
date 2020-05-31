1dbe53db50912390db7cf468fa107c862f796049
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 21:32:15 +0530

    modules

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-var Point = /** @class */ (function () {
-    function Point(_x, _y) {
-        this._x = _x;
-        this._y = _y;
-    }
-    Point.prototype.draw = function () {
-        console.log("X: " + this._x + ", Y: " + this._y);
-    };
-    Object.defineProperty(Point.prototype, "x", {
-        get: function () {
-            return this._x;
-        },
-        set: function (value) {
-            if (value < 0)
-                throw new Error("value cannot be less than 0.");
-            this._x = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Point;
-}());
-var point = new Point(1, 2);
-var x = point.x;
-point.x = 10;
+"use strict";
+exports.__esModule = true;
+var point_1 = require("./point");
+var point = new point_1.Point(1, 2);
 point.draw();
