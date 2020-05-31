ff0b723c3adf15354b91b52c8f7be598b0e4c7cb
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 20:54:41 +0530

    type assertions for intellisense of type 'any'

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[] = [1, 2, 3];
-let f: any[] = [1, true, "a", false];
-
-enum color {
-  Red = 0,
-  Green = 1,
-  Blue = 2,
-}
-
-let backgroundColor = color.Red;
+let message;
+message = "abc";
+let endsWithC = (<string>message).endsWith("c");
+let alternativeWay = (message as string).endsWith("c");
