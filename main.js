159b247b4a4edb79f5b80d5009bda93fa9b7f810
Sumit Yadav <ysumit99@gmail.com>
2020-05-31 21:11:23 +0530

    creating objects in ts

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var a;
-var b;
-var c;
-var d;
-var e = [1, 2, 3];
-var f = [1, true, "a", false];
-var color;
-(function (color) {
-    color[color["Red"] = 0] = "Red";
-    color[color["Green"] = 1] = "Green";
-    color[color["Blue"] = 2] = "Blue";
-})(color || (color = {}));
-var backgroundColor = color.Red;
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log("X: " + this.x + ", Y: " + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+        //...
+    };
+    return Point;
+}());
+var point = new Point();
+point.x = 1;
+point.y = 3;
+point.draw();
--- main.ts
+++ main.ts
@@ -2,10 +2,15 @@ class Point {
   x: number;
   y: number;
   draw() {
-    //...
+    console.log("X: " + this.x + ", Y: " + this.y);
   }
 
   getDistance(another: Point) {
     //...
   }
 }
+
+let point = new Point();
+point.x = 1;
+point.y = 3;
+point.draw();
